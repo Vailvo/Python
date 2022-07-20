a921d7e565440275ca9db256ead37bb787883e0d
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-07-20 15:56:32 -0700

    converting and adding

---
+++ dataTypes/excercise.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# first finding out what data type two_digit_number is 
+# then seperating by index and converting the data type to achieve result.
+# print(two_digit_number[0])
+# print(type(two_digit_number))
+num_1 = int(two_digit_number[0])
+num_2 = int(two_digit_number[1])
+
+print(num_1 + num_2)
