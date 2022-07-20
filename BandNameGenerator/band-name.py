21b511f648b5a0f446c16fa00214c48cc7c922e9
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-07-19 19:58:07 -0700

    first commit

---
+++ BandNameGenerator/band-name.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Generator\nWhat is your favorite color?")
+
+str1 = input()
+print("What is your favorite food?")
+str2 = input()
+print("Your band name should be " + str1 + " " + str2 + "!")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
