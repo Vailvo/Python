7cfa52c9b3312d5b0498fb423795b6d4e4ba6328
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-07-27 16:27:26 -0700

    tip of each added

--- dataTypes/exercise4.py
+++ dataTypes/exercise4.py
@@ -13,12 +13,13 @@ tip = int(input("How much would you like to tip? 12%, 15%, 25% ?\n"))
 # converting tip variable from input string into an integer(whole number)
 total_with_tip = tip / 100 * bill + bill
 # dividing tip by 100 to get the decimal value of tip then multiplying by bill for the percentage of tip. Adding the bill to get the total bill amount with tip.
-
+tip_each = tip / 100 * bill
+tip_each = "{:.2f}". format(tip_each)
 split_bill = int(input("How many people to split the bills?\n"))
 # splitting the bill with how many people
 final_bill = round(total_with_tip / split_bill, 2)
 # final_bill is rounded with the method round, total_with_tip is divided by split_bill(number of people) and the int value of 2 to move the decimal 2 places to the right.
 final_bill = "{:.2f}". format(final_bill)
 # using this trick there will always be 2 decimal places
-print(f"Each person should pay ${final_bill}.")
+print(f"Each person should pay ${final_bill} with a tip each of ${tip_each}")
 # final output ex: $00.00
