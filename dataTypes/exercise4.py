8d2b7b8ccc2b0ea0d515d80d8fc858ac10fdebea
Shazé Williams <44694518+Vailvo@users.noreply.github.com>
2022-10-08 15:36:47 -0700

    new conditions and nested if statements

--- dataTypes/exercise4.py
+++ dataTypes/exercise4.py
@@ -11,11 +11,14 @@ bill = float(input("What was the total bill? $"))
 # coverting the bill to a float and storing input as bill variable
 tip = int(input("How much would you like to tip? 12%, 15%, 25% ?\n"))
 # converting tip variable from input string into an integer(whole number)
+
 total_with_tip = tip / 100 * bill + bill
 # dividing tip by 100 to get the decimal value of tip then multiplying by bill for the percentage of tip. Adding the bill to get the total bill amount with tip.
 tip_each = tip / 100 * bill
 tip_each = "{:.2f}". format(tip_each)
 split_bill = int(input("How many people to split the bills?\n"))
+each_per = split_bill / tip_each
+print(round(each_per, 2))
 # splitting the bill with how many people
 final_bill = round(total_with_tip / split_bill, 2)
 # final_bill is rounded with the method round, total_with_tip is divided by split_bill(number of people) and the int value of 2 to move the decimal 2 places to the right.
---
+++ dataTypes/exercise5.py
@@ -0,0 +1,9 @@
+# Major Purchase Calculator
+print("Welcome to the Major Purchase Calculator!~")
+type_of_purchase = input("What major purchase are you planning to buy? Car or House?\n")
+
+if type_of_purchase == "Car" or "car":
+    print("You chose a car")
+else:
+    type_of_purchase == "House" or "house"
+    print("You chose a house")
